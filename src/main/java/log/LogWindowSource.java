6c221e660a532ff818738a78f0c0af767aa00fae
Scope <scopeekb@yandex.ru>
2020-03-11 22:37:39 +0500

    Add queue for log messages

--- src/main/java/log/LogWindowSource.java
+++ src/main/java/log/LogWindowSource.java
@@ -3,26 +3,23 @@ package log;
 import java.util.ArrayList;
 import java.util.Collections;
 
-/**
- * Что починить:
- * 1. Этот класс порождает утечку ресурсов (связанные слушатели оказываются
- * удерживаемыми в памяти)
- * 2. Этот класс хранит активные сообщения лога, но в такой реализации он
- * их лишь накапливает. Надо же, чтобы количество сообщений в логе было ограничено
- * величиной m_iQueueLength (т.е. реально нужна очередь сообщений
- * ограниченного размера)
- */
-public class LogWindowSource {
-    private int m_iQueueLength;
+import java.util.concurrent.*;
 
-    private ArrayList<LogEntry> messages;
+/**
+ * TODO:
+ *  Этот класс порождает утечку ресурсов (связанные слушатели оказываются
+*/
+public class LogWindowSource {
+    private int queueLength;
+
+    private ConcurrentLinkedQueue<LogEntry> messages;
     private final ArrayList<LogChangeListener> listeners;
     private volatile LogChangeListener[] activeListeners;
 
-    public LogWindowSource(int iQueueLength) {
-        m_iQueueLength = iQueueLength;
-        messages = new ArrayList<LogEntry>(iQueueLength);
-        listeners = new ArrayList<LogChangeListener>();
+    public LogWindowSource(int queueLength) {
+        this.queueLength = queueLength;
+        messages = new ConcurrentLinkedQueue<>();
+        listeners = new ArrayList<>();
     }
 
     public void registerListener(LogChangeListener listener) {
@@ -41,6 +38,8 @@ public class LogWindowSource {
 
     public void append(LogLevel logLevel, String strMessage) {
         LogEntry entry = new LogEntry(logLevel, strMessage);
+        if (messages.size() == queueLength)
+            messages.poll();
         messages.add(entry);
         LogChangeListener[] activeListeners = this.activeListeners;
         if (activeListeners == null) {
@@ -65,10 +64,20 @@ public class LogWindowSource {
             return Collections.emptyList();
         }
         int indexTo = Math.min(startFrom + count, messages.size());
-        return messages.subList(startFrom, indexTo);
+        return subSequence(startFrom, indexTo);
     }
 
     public Iterable<LogEntry> all() {
         return messages;
+    }
+
+    private Iterable<LogEntry> subSequence(int startFrom, int indexTo) {
+        ArrayList<LogEntry> entries = new ArrayList<>();
+        int count = 0;
+        for (LogEntry entry : messages) {
+            if (count >= startFrom && count <= indexTo)
+                entries.add(entry);
+        }
+        return entries;
     }
 }
